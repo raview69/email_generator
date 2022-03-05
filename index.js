3742370e28b2c33265cb7ce73c018adcdbc289eb
raview69 <raviwicaksono@aol.com>
2022-03-05 08:56:51 +0700

    fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import fetch from "node-fetch";
+import fs from "fs";
+import fse from "fs-extra";
 
+// getting time
+let date_ob = new Date();
+let date = "0" + date_ob.getDate();
+let month = "0" + (date_ob.getMonth() + 1);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+
+// time to create folder
+const time = `${year}-${month}-${date} ${hours}:${minutes}`;
+
+// get api json
 const url = "https://api.datamuse.com/words?";
 const queryParams = "rel_jja=";
 
@@ -10,7 +24,8 @@ const getdata = async () => {
 		const response = await fetch(endpoint);
 		if (response.ok) {
 			const jsonResponse = await response.json();
-			console.log(jsonResponse);
+			const resjson = JSON.stringify(jsonResponse);
+			fse.outputFileSync(`${time}/jembut.js`, `${resjson}`);
 		}
 	} catch (error) {
 		console.log(error);
@@ -18,3 +33,4 @@ const getdata = async () => {
 };
 
 getdata();
+console.log("create file successfully");
